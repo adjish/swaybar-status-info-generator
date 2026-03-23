c5563b6704bd783d7d595000f385eacc6b71a232
adjish <adjish3@gmail.com>
2026-03-23 22:39:15 +0100

    Fix

--- main.c
+++ main.c
@@ -28,7 +28,12 @@ int main(void)
         printf("| %02d:%02d %s %02d/%02d/%02d\n", timeinfo->tm_hour, timeinfo->tm_min, day_of_week, timeinfo->tm_mday,
                timeinfo->tm_mon + 1, timeinfo->tm_year % 100);
 
-        fflush(stdout);
+        if (fflush(stdout) == EOF)
+        {
+            fprintf(stderr, "flush error: %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+
         usleep(SLEEP_TIME);
     }
 
